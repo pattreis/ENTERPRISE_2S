e61a758cf654209526a2b34dc837b552d6ed28f5
Logon Aluno <logonrmlocal@fiap.com.br>
2018-08-13 22:53:00 -0300

    03 mvc

---
+++ 03.fiap.web.mvc/03.fiap.web.mvc/Controllers/FilmeController.cs
@@ -0,0 +1,36 @@
+﻿using _03.fiap.web.mvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _03.fiap.web.mvc.Controllers
+{
+    public class FilmeController : Controller
+    {
+
+        // Post: Filme
+        [HttpGet]
+        public ActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Cadastrar(Filme filme)
+        {
+            //Passar valores para a view (pagina html)           
+            ViewBag.titulo = filme.Nome;
+            //Passar uma mensagem para a view
+            TempData["msg"] = "Filme Cadastrado!";
+
+            //Passar um objeto para a view
+            return View(filme);
+            //Retornar um texo para o browser
+            //return Content(filme.Nome);            
+        }
+
+        
+    }
+}
---
+++ 03.fiap.web.mvc/03.fiap.web.mvc/Models/Filme.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _03.fiap.web.mvc.Models
+{
+    public class Filme
+    {
+        public string Nome { get; set; }
+
+        public DateTime DtLancamento { get; set; }
+
+        public string Genero { get; set; }
+
+        public bool Legendado { get; set; }
+    }
+}
