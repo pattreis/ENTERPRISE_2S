df55a9c17a569e8fa5841cbbccd63e19731f7c8f
Logon Aluno <logonrmlocal@fiap.com.br>
2018-09-10 20:45:08 -0300

    mvc cad produto

---
+++ 06.Fiap.Web.MVC/06.Fiap.Web.MVC/Controllers/ProdutoController.cs
@@ -0,0 +1,25 @@
+﻿using _06.Fiap.Web.MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _06.Fiap.Web.MVC.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        // GET: Produto
+        [HttpGet]
+        public ActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Cadastrar(Produto produto)
+        {
+            return View();
+        }
+    }
+}
--- 06.Fiap.Web.MVC/06.Fiap.Web.MVC/Models/Produto.cs
+++ 06.Fiap.Web.MVC/06.Fiap.Web.MVC/Models/Produto.cs
@@ -1,15 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
-namespace _06.Fiap.Web.MVC.Persistencia
+namespace _06.Fiap.Web.MVC.Models
 {
+    [Table("TProduto")]
     public class Produto
     {
+        [Column("Id")]
         public int ProdutoId { get; set; }
 
+        [Required]
+        [MaxLength(200)]
         public string Nome { get; set; }
-
     }
 }
